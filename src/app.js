9d918e4762c69537c6bcbf2fc5587274b7267594
Sabrinaprogramadora <chabina1@gmail.com>
2022-03-15 22:26:46 -0300

    puerto

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "../","public")));
 
@@ -16,4 +16,4 @@ app.get("/", (req, res) =>
 
 
 
-app.listen(port, () => console.log("estoy funcionando en el puerto " + port));
+app.listen(PORT, () => console.log("estoy funcionando en el puerto " + port));
